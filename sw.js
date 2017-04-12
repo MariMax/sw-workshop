04c964bb7a403590e9c6077517425db5acf4bdff
Maxim Milovanov <mma29121983@gmail.com>
2017-04-12 16:12:05 -0400

    sw update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'cache-v1';
+var CACHE_NAME = 'cache-v2';
 var filesToCache = [
   '/',
   '/index.html',
@@ -15,41 +15,22 @@ self.addEventListener('install', (event) => event.waitUntil(
     .then(cache => cache.addAll(filesToCache))
 ));
 
-// // cache with network fallback
-// self.addEventListener('fetch', (event) => event.respondWith(
-//     caches.match(event.request)
-//       .then(response => response || fetch(event.request)
-//     )
-//   ));
-
-//network with cache fallback
+// cache with network fallback
 self.addEventListener('fetch', (event) => event.respondWith(
-  fetch(event.request)
-    .then(response => {
-      if (response.status >= 200 && response.status < 400) return response;
-      return caches.match(event.request)
-    })
+  caches.match(event.request)
+    .then(response => response || fetch(event.request)
+    )
 ));
 
-// //network/cache race
-// function promiseAny(promises) {
-//   return new Promise((resolve, reject) => {
-//     // make sure promises are all promises
-//     promises = promises.map(p => Promise.resolve(p));
-//     // resolve this promise as soon as one resolves
-//     promises.forEach(p => p.then(resolve));
-//     // reject if all promises reject
-//     promises.reduce((a, b) => a.catch(() => b))
-//       .catch(() => reject(Error("All failed")));
-//   });
-// };
-
-// self.addEventListener('fetch', (event) => event.respondWith(
-//   promiseAny([
-//       caches.match(event.request),
-//       fetch(event.request)
-//     ])
-// ));
-
-// cache first network later
-// generic offline response
+self.addEventListener('activate', (event) => {
+  return event.waitUntil(
+    caches.keys()
+      .then(cacheNames => Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      ))
+  )
+});
